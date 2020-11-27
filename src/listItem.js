60b2f702f14cf5a4c4f0ceecfa01ffa2dabe434e
Nosov-Pavel <nopaev@gmail.com>
2020-11-27 15:53:26 -0500

    listItem

--- src/listItem.js
+++ src/listItem.js
@@ -2,51 +2,25 @@ import React, {useState} from "react";
 import axios from 'axios';
 import ListItem from "./listItem";
 
-function App(){
-    const [List, setList] = useState([]);
-     const getList = () => {
-         axios.get('http://nazarov-kanban-server.herokuapp.com/card')
-             .then(res => {
-                 setList(res.data);
+function ListItem(props){
+    const {el} = props;
 
-             })
-             .catch(error => {
-                 console.log(error)
-
-             })
-
-
-     }
-     const deleteCard = async (id) => {
-         await axios.delete(`http://nazarov-kanban-server.herokuapp.com/card/${id}`)
-             .then(res => console.log(res))
-             .catch(err => console.log(err))
-
-     }
-     const createCard = () => {
-         axios.post('http://nazarov-kanban-server.herokuapp.com/card/',{
-             name: 'hello',
-             description: 'bye'
-         })
-             .then(res => console.log(res))
-             .catch(err => console.log(err))
-
-     }
 
     return(
         <div>
-            <button onClick={createCard}>create</button>
-            <button onClick={getList}>get</button>
-            <hr/>
-            {list.map(el =>
-                <ListItem el={el} deleteCard={deleteCard}/>
+            <li>
+                {el.name}
+                {' '}
+                {el.description}
 
+                <button onClick={() =>props.deleteCard(el._id)}>delete</button>
+            </li>)}
 
         </div>
     );
 }
 
-export default App;
+export default ListItem;
 
 
 
