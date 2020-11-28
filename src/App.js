447a2c0c5671737ac95e873d0dc3db1544226784
Nosov-Pavel <nopaev@gmail.com>
2020-11-28 07:59:52 -0500

    button+

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function App(){
+function App() {
 
-    return(
+    return (
         <div>
             6
+            <button>+</button>
 
         </div>
     );
